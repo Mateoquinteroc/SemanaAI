da94676772edf3e64b8f412ffe80f0cea4da03bf
Mateo Quintero C <85648547+Mateoquinteroc@users.noreply.github.com>
2024-06-07 15:10:20 -0500

    logo boton

--- src/Boton.tsx
+++ src/Boton.tsx
@@ -1,26 +1,20 @@
+import logoyou from '../public/you-tube-logo-06.svg'; 
+import './fondo.css'
+
 
 const Boton = () => {
   const handleClick = () => {
     window.location.href = 'https://www.youtube.com/@CentroDeCienciaFJC/streams'; // Cambia la URL a la que desees
   };
 
-  const buttonStyle = {
-    position: 'fixed',
-    top: '10px',
-    right: '10px',
-    padding: '10px 20px',
-    backgroundColor: '#c4302b',
-    color: 'white',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    zIndex: 1000,
-  };
 
   return (
-    <button style={buttonStyle} onClick={handleClick}>
+    <div className="butonContainer">
+      <button className="buttonStyle" onClick={handleClick}>
+        <img  className="logoyou" src={logoyou} />
       Live
-    </button>
+      </button>
+    </div>
   );
 };
 
